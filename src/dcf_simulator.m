7729138f9e60b1ea669e61a31ebd38c77fe79102
Christopher Wood <christopherwood07@gmail.com>
2015-01-17 20:58:30 -0800

    Simulator done

--- src/dcf_simulator.m
+++ src/dcf_simulator.m
@@ -1,8 +1,8 @@
 %%% DCF Monte Carlo simulator
 
 % Parameters
-timeSteps = 1000;
-p = 0.5;
+timeSteps = 100000;
+p = 0.5; % conditional collision probability
 Wmin = 2;
 Wmax = 4; %1024;
 m = log2(Wmax / Wmin);
@@ -12,9 +12,36 @@ for i = 1:(m+1)
 end
 
 % State variables
-backoffTimer = 0;
-backoffStage = 0;
+timer = 0;
+stage = 0;
 
-for t = 1:timeSteps
-   disp('Time step 
+% Statistical accumulator variables
+successes = 0;
+failures = 0;
+
+% Initialize the state
+%   - pick random backoff stage
+timer = floor(rand(1) * W(1,1));
+
+for T = 1:timeSteps
+   if (backoffTimer > 0)
+      k = k - 1; 
+   else
+       txSuccess = rand(1) < (1 - p); 
+       if (txSuccess == 1) % if success, go to stage 0 with random backoff k
+          successes = successes + 1;
+          stage = 0;
+          timer = floor(rand(1) * W(1,1));
+       else % collision, go to the next backoff stage and choose a new random timer
+           failures = failures + 1;
+           if (stage ~= m) % loop at stage m
+              stage = stage + 1; 
+           end
+           timer = floor(rand(1) * W(1,stage));
+       end
+   end
 end
+
+% Results
+disp(sprintf('Successes =  %d', successes))
+disp(sprintf('Failures =  %d', failures))
