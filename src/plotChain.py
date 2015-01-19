6d14e7dfb38016562be3eabd939a0224794dd6a4
Christopher Wood <christopherwood07@gmail.com>
2015-01-19 11:28:10 -0800

    cmdline

--- src/plotChain.py
+++ src/plotChain.py
@@ -4,13 +4,15 @@ import scipy.io
 import matplotlib.pyplot as plt
 
 def main(args):
-	pi = scipy.io.loadmat('pi.mat')
-	dims = scipy.io.loadmat('dims.mat')
+	pi = scipy.io.loadmat(args[1])
+	dims = scipy.io.loadmat(args[2])
 
-	print dims["dims"]
 	total = 1
 	for i in range(len(dims["dims"][0])):
 		total = total * dims["dims"][0][i]
+
+	# Debug
+	print dims["dims"]
 	print total
 	print pi["pi"]
 
