f91ffd1c8a3bc4bc994bb7f7443bc25c15c079bb
Tamir H <tamirh@gmail.com>
2015-02-02 10:46:02 -0800

    Filling out methods for the DCF container class

--- src/markov/dcf_container.m
+++ src/markov/dcf_container.m
@@ -3,12 +3,36 @@ classdef dcf_container < handle
     
     properties (SetAccess = protected)
         % HashTable of all states
+        % key: dimensioned index
+        % value: handle to dcf_state object
         states@containers.Map = containers.Map();    
     end
     
     methods
+        % Return the probability of state->state transition (src->dst)
+        % srcKey: dimensioned index
+        % dstKey: dimensioned index
+        function p = P(this, srcKey, dstKey)
+            p = 0;
+            
+            if (this.states.isKey(srcKey) && this.states.isKey(dstKey))
+                srcState = this.states(srcKey);
+                
+                if (srcState.P.isKey(dstKey))
+                    p = srcState.P(dstKey);
+                end
+            end
+        end
         
-    end
-    
+        % Set the probability of state->state transition (src->dst)
+        % srcKey: dimensioned index
+        % dstKey: dimensioned index
+        % p: probability [0,1]
+        function setP(this, srcKey, dstKey, p)
+            assert(this.states.isKey(srcKey));
+            
+            srcState = this.states(srcKey);
+            srcState.P(dstKey) = p;
+        end
+    end 
 end
-
