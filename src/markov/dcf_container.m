521e16aa4bfd39024151b883fdb489011d4a7cab
Tamir H <tamirh@gmail.com>
2015-02-02 11:17:21 -0800

    Adding logic for flattening and for outputting transition table

--- src/markov/dcf_container.m
+++ src/markov/dcf_container.m
@@ -5,7 +5,7 @@ classdef dcf_container < handle
         % HashTable of all states
         % key: dimensioned index
         % value: handle to dcf_state object
-        states@containers.Map = containers.Map();    
+        states@containers.Map = containers.Map('KeyType',int32, 'ValueType',dcf_state);   
     end
     
     methods
@@ -33,6 +33,39 @@ classdef dcf_container < handle
             
             srcState = this.states(srcKey);
             srcState.P(dstKey) = p;
+        end
+        
+        % Flatten all of the states and give them a 1d index
+        function flatten(this)
+            % TODO: Sort this so it comes out in a logical order
+            valueSet = values(this.states);
+            nStates = size(valueSet,1);
+            
+            for i=1:nStates
+                valueSet(i).IF = i;
+            end
+        end
+        
+        % Convert the states into a transition table
+        function t = transitionTable(this)
+            valueSet = values(this.states);
+            nStates = size(valueSet, 1);
+            t = zeros(nStates, nStates);
+            
+            % For all source states
+            for i=1:nStates
+                src = valueSet(i);
+                dstSet = values(src.P);
+                nDst = size(dstSet, 1);
+                
+                % For all destination states from this srouce
+                for j=1:nDst
+                    dst = dstSet(j);
+                    
+                    % Set the probability in the 2d transition table
+                    t(src.IF, dst.IF) = src.P(dst);
+                end
+            end
         end
     end 
 end
