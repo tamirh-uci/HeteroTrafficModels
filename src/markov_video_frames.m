2b3e42555773c7522609981d5b33ef9d727f7fd9
Tamir <tamirh@gmail.com>
2015-02-23 19:34:08 -0800

    deterministic markov model of GOP frames

--- src/markov_video_frames.m
+++ src/markov_video_frames.m
@@ -6,16 +6,21 @@ classdef markov_video_frames < handle
             key = [int32(type) indices];
         end
         
+        % There is one i-frame per GOP (we don't count the end bookend,
+        % because we just consider that part of the next GOP for markov
+        % chain reasons)
         function key = IFrameState()
             key = markov_video_frames.Dim(dcf_state_type.IFrame, 0);
         end
         
-        function key = BFrameState()
-            key = markov_video_frames.Dim(dcf_state_type.BFrame, 0);
+        % indices = [group, countdown]
+        function key = BFrameState(indices)
+            key = markov_video_frames.Dim(dcf_state_type.BFrame, indices);
         end
         
-        function key = PFrameState()
-            key = markov_video_frames.Dim(dcf_state_type.PFrame, 0);
+        % indices = [group]
+        function key = PFrameState(indices)
+            key = markov_video_frames.Dim(dcf_state_type.PFrame, indices);
         end
     end % methods (Static)
     
@@ -36,89 +41,68 @@ classdef markov_video_frames < handle
         end
         
         function CalculateConstants(this)
-            % Dump into a matrix to calculate easier
-            pi = [this.pII, this.pIB, this.pIP; this.pBI, this.pBB, this.pBP; this.pPI, this.pPB, this.pPP];
-            epsilon = 0.0001;
-            
-            for row=1:3
-                z = find( pi(row,:)==0 );
-                nonz = find( pi(row,:)~=0 );
-                nZero = size(z,2);
-                
-                % Divide equally if all 3 gone
-                if( nZero == 3 )
-                    pi(row, z) = 1.0/3.0;
-                elseif (nZero == 2)
-                    % Divide between both 0's if you only have 1 p
-                    r = 1.0 - pi(row,nonz);
-                    pi(row,z) = 0.5 * r;
-                elseif (nZero == 1)
-                    % Remainder goes to 0 p
-                    r = 1.0 - sum( pi(row,nonz) );
-                    pi(row,z) = r;
-                end
-            end
-            
-            for row=1:3
-                s = sum( pi(row,:) );
-                assert( s >= -epsilon );
-                assert( s <= 1.0 + epsilon );
-            end
-            
-            % dump values back into variables for easy use
-            this.pII = pi(1,1);
-            this.pIB = pi(1,2);
-            this.pIP = pi(1,3);
-            
-            this.pBI = pi(2,1);
-            this.pBB = pi(2,2);
-            this.pBP = pi(2,3);
-            
-            this.pPI = pi(3,1);
-            this.pPB = pi(3,2);
-            this.pPP = pi(3,3);            
+            this.nGroups = int32( floor(this.gopFullFrameDistance / this.gopAnchorFrameDistance) );
+            this.nBPerGroup = this.gopAnchorFrameDistance - 1;
         end
         
         function GenerateStates(this, chain)
-            chain.NewState( dcf_state( this.IFrameState(), dcf_state_type.IFrame ) );
-            chain.NewState( dcf_state( this.BFrameState(), dcf_state_type.BFrame ) );
-            chain.NewState( dcf_state( this.PFrameState(), dcf_state_type.PFrame ) );
+            % Single i frame will lead the GOP
+            chain.NewState( dcf_state( IFrameState(), dcf_state_type.IFrame ) );
+            
+            % Repeat pattern of BBP
+            for group = 1:this.nGroups
+                % Repition of B's
+                for bCount = 1:this.nBPerGroup
+                    chain.NewState( dcf_state( BFrameState([group, bCount]), dcf_state_type.BFrame ) );
+                end
+                
+                % P frames cap off the group of B's, unless it's the last
+                if (group < this.nGroups)
+                    chain.NewState( dcf_state( PFrameState(group), dcf_state_type.PFrame ) );
+                end
+            end
         end
         
         function SetProbabilities(this, chain)
-            % Simple 3 state markov chain, each state has p to every other
-            iFrame = this.IFrameState();
-            bFrame = this.BFrameState();
-            pFrame = this.PFrameState();
+            % Single i frame will lead the GOP
+            src = IFrameState();
             
-            chain.SetP( iFrame, iFrame, this.pII, dcf_transition_type.TxIFrame );
-            chain.SetP( iFrame, bFrame, this.pIB, dcf_transition_type.TxBFrame );
-            chain.SetP( iFrame, pFrame, this.pIP, dcf_transition_type.TxPFrame );
-            
-            chain.SetP( bFrame, iFrame, this.pBI, dcf_transition_type.TxIFrame );
-            chain.SetP( bFrame, bFrame, this.pBB, dcf_transition_type.TxBFrame );
-            chain.SetP( bFrame, pFrame, this.pBP, dcf_transition_type.TxPFrame );
-            
-            chain.SetP( pFrame, iFrame, this.pPI, dcf_transition_type.TxIFrame );
-            chain.SetP( pFrame, bFrame, this.pPB, dcf_transition_type.TxBFrame );
-            chain.SetP( pFrame, pFrame, this.pPP, dcf_transition_type.TxPFrame );
+            % Repeat pattern of BBP
+            for group = 1:this.nGroups
+                % Repition of B's
+                for bCount = 1:this.nBPerGroup
+                    dst = BFrameState([group, bCount]);
+                    chain.SetP( src, dst, 1.0, dcf_transition_type.BFrame );
+                    src = dst;
+                end
+                
+                % P frames cap off the group of B's, unless it's the last
+                if (group < this.nGroups)
+                    dst = PFrameState(group);
+                    chain.SetP( src, dst, 1.0, dcf_transition_type.PFrame );
+                    src = dst;
+                else
+                    dst = IFrameState();
+                    chain.SetP( src, dst, 1.0, dcf_transition_type.IFrame );
+                    src = dst;
+                end
+            end
         end
     end % methods
     
-    % Transition probabilities from each frame type to each other
+    % GOP characteristics
     properties
-        pII = 0;
-        pIB = 0;
-        pIP = 0;
+        % One minus the distance between two P frames in a GOP
+        gopAnchorFrameDistance@int32 = 3;
         
-        pBI = 0;
-        pBB = 0;
-        pBP = 0;
+        % Distance between two I frames (length of a GOP less the bookend I)
+        gopFullFrameDistance@int32 = 12;
         
-        pPI = 0;
-        pPB = 0;
-        pPP = 0;
-    end % properties
-    
+        % Number of BP groups
+        nGroups@int32;
+        
+        % Number of B frames per group
+        nBPerGroup@int32;
+    end   
 end
 
