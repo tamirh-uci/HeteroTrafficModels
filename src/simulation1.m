d0691c3e2585e6f685c402aa6bd4b5e1901c89ef
Christopher Wood <christopherwood07@gmail.com>
2015-02-26 12:09:16 -0800

    sim

--- src/simulation1.m
+++ src/simulation1.m
@@ -1,26 +1,49 @@
-timeSteps = 100000; 
-pSuccess = 0.25; 
-pArrive = 1;
+% Simulation parameters
+timeSteps = 10000; 
+numberOfNodes = 1;
+pSuccess = 1.0; 
+pArrive = 1.0;
 pEnter = 0;
-Wmin = 2;
-Wmax = 4; %1024;
+
+% PHY layer parameters -- using FHSS (frequency hopping spread spectrum)
+Wmin = 8;
+Wmax = 32;
+
+% Precompute variables for the DCF model
 m = log2(Wmax / Wmin);
 W = zeros(1,m+1);
 for i = 1:(m+1)
     W(1,i) = (2^(i-1)) * Wmin;
 end
 
-% create the simulator
-simulator = dcf_simulator_oo(pSuccess, 0);
-
-% create the nodes we want to participate in the sim
-n1 = dcf_matrix_factory(pArrive, pEnter, m, Wmin, 1, 0);
-n2 = dcf_matrix_factory(pArrive, pEnter, m, Wmin, 1, 0);
-simulator.add_dcf_matrix('n1', n1);
-simulator.add_dcf_matrix('n2', n2);
-
-simulator.Setup();
-
-% run the simulation
-simulator.Steps(1000);
-simulator.PrintResults();
+% Run the different series of simulations
+for i = 1:numberOfNodes
+    simulator = dcf_simulator_oo(pSuccess, 0);
+    for j = 1:i
+        node = dcf_matrix_factory(pArrive, pEnter, m, Wmin, 1, 0);
+        nodeName = sprintf('node%d', j);
+        simulator.add_dcf_matrix(nodeName, node);
+    end
+    simulator.Setup();
+    simulator.Steps(timeSteps);
+    simulator.PrintResults();
+    
+    % open the output results file
+    fName = sprintf('sim_data_%d', i);
+    fid = fopen(fName, 'w');
+    if (fid == -1)
+        disp('Error: could not open the file for output.');
+        exit;
+    end
+    
+    % write the simulation parameters and results for each node to the file
+    fprintf(fid, '%d,%d,%f,%f,%f,%d,%d\n', timeSteps, i, pSuccess, pArrive, pEnter, Wmin, Wmax);
+    for j = 1:i
+        node = simulator.GetNode(j);
+        fprintf(fid, '%d,%d,%d\n', node.CountSuccesses(), node.CountFailures(), node.CountWaits());
+    end
+    fprintf(fid, '%d,%d,%d\n', simulator.CountSuccesses(), simulator.CountFailures(), simulator.CountWaits());
+    
+    % flush and cleanup
+    fclose(fid);
+end
