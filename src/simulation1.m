6e847692da7ccb47a0eb1b53ab28a9a849431d9a
Tamir H <tamirh@gmail.com>
2015-02-26 14:13:19 -0800

    pulling out simulation from the setup loop. only need to execute it once

--- src/simulation1.m
+++ src/simulation1.m
@@ -24,26 +24,27 @@ for i = 1:numberOfNodes
         nodeName = sprintf('node%d', j);
         simulator.add_dcf_matrix(nodeName, node);
     end
-    simulator.Setup();
-    simulator.Steps(timeSteps);
-    simulator.PrintResults(false);
-    
-    % open the output results file
-    fName = sprintf('sim_data_%d', i);
-    fid = fopen(fName, 'w');
-    if (fid == -1)
-        disp('Error: could not open the file for output.');
-        exit;
-    end
-    
-    % write the simulation parameters and results for each node to the file
-    fprintf(fid, '%d,%d,%f,%f,%f,%d,%d\n', timeSteps, i, pSuccess, pArrive, pEnter, Wmin, Wmax);
-    for j = 1:i
-        node = simulator.GetNode(j);
-        fprintf(fid, '%d,%d,%d\n', node.CountSuccesses(), node.CountFailures(), node.CountWaits());
-    end
-    fprintf(fid, '%d,%d,%d\n', simulator.CountSuccesses(), simulator.CountFailures(), simulator.CountWaits());
-    
-    % flush and cleanup
-    fclose(fid);
 end
+
+simulator.Setup();
+simulator.Steps(timeSteps, true);
+simulator.PrintResults(false);
+
+% open the output results file
+fName = sprintf('sim_data_%d', i);
+fid = fopen(fName, 'w');
+if (fid == -1)
+    disp('Error: could not open the file for output.');
+    exit;
+end
+
+% write the simulation parameters and results for each node to the file
+fprintf(fid, '%d,%d,%f,%f,%f,%d,%d\n', timeSteps, i, pSuccess, pArrive, pEnter, Wmin, Wmax);
+for j = 1:i
+    node = simulator.GetNode(j);
+    fprintf(fid, '%d,%d,%d\n', node.CountSuccesses(), node.CountFailures(), node.CountWaits());
+end
+fprintf(fid, '%d,%d,%d\n', simulator.CountSuccesses(), simulator.CountFailures(), simulator.CountWaits());
+
+% flush and cleanup
+fclose(fid);
