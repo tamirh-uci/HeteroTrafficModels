579c1e3420cf8b439c5a06317670ee08951ae52a
Tamir H <tamirh@gmail.com>
2015-03-07 16:10:51 -0800

    Adding some extra helper functions to phys 802.11 class

--- src/phys80211.m
+++ src/phys80211.m
@@ -15,45 +15,50 @@ classdef phys80211 < handle
         % What is the maximum datarate of a DCF given no other nodes
         % type = the type physical type of 802.11 transmission
         % wMin = number of columns/states in the first stage of DCF
-        % speed = [0,1] linear combination of min-max speed of the type
+        % speed = [0,1] %capacity of max
         % rate = (bits/second)
-        function rate = MaxDatarate(type, wMin, speed)
+        function rate = MaxSingleNodeDatarate(type, wMin, speed)
             transmitTime = phys80211.SlotTime(type);
             backoffTime = (wMin-1)*phys80211.SIFS(type);
             percentTransmitting = transmitTime / (transmitTime + backoffTime);
             
-            [datarateMin, datarateMax] = phys80211.RawDatarate(type);
-            rawDatarate = speed * (datarateMax-datarateMin) + datarateMin;
-            
-            rate = rawDatarate * percentTransmitting;
+            rate = phys80211.RawDatarate(type, speed) * percentTransmitting;
+        end
+        
+        % how fast individual transmissions send
+        % given the system is working at %capacity of max
+        % speed = [0,1] %capacity of max
+        function datarate = RawDatarate(type, speed)
+            [min, max] = RawMinMaxDatarate(type);
+            datarate = speed * (max-min) + min;
         end
         
         % how fast individual transmissions send 
         % unit = [min, max] (bits/second)
-        function [min, max] = RawDatarate(type)
+        function [min, max] = RawMinMaxDatarate(type)
             switch(type)
-                case phys_80211_type.FHSS % 1-2 Mbit
+                case phys80211_type.FHSS % 1-2 Mbit
                     min = 1000000;
                     max = 2000000;
-                case phys_80211_type.DHSS % 1-2 Mbit
+                case phys80211_type.DHSS % 1-2 Mbit
                     min = 1000000;
                     max = 2000000;
-                case phys_80211_type.B % 1-11 Mbit
+                case phys80211_type.B % 1-11 Mbit
                     min = 1000000;
                     max = 11000000;
-                case phys_80211_type.A % 1.5-54 Mbit
+                case phys80211_type.A % 1.5-54 Mbit
                     min = 1500000;
                     max = 54000000;
-                case phys_80211_type.G % 1-54 Mbit
+                case phys80211_type.G % 1-54 Mbit
                     min = 1000000;
                     max = 54000000;
-                case phys_80211_type.N24 % 1-600 Mbit
+                case phys80211_type.N24 % 1-600 Mbit
                     min = 1000000;
                     max = 600000000;
-                case phys_80211_type.N50 % 1-600 Mbit
+                case phys80211_type.N50 % 1-600 Mbit
                     min = 1000000;
                     max = 600000000;
-                case phys_80211_type.AC % 1-500 Mbit
+                case phys80211_type.AC % 1-500 Mbit
                     min = 1000000;
                     max = 500000000;
             end
@@ -62,21 +67,21 @@ classdef phys80211 < handle
         % unit = (microseconds)
         function sifs = SIFS(type)
             switch(type)
-                case phys_80211_type.FHSS
+                case phys80211_type.FHSS
                     sifs = 28;
-                case phys_80211_type.DHSS
+                case phys80211_type.DHSS
                     sifs = 10;
-                case phys_80211_type.B
+                case phys80211_type.B
                     sifs = 10;
-                case phys_80211_type.A
+                case phys80211_type.A
                     sifs = 16;
-                case phys_80211_type.G
+                case phys80211_type.G
                     sifs = 10;
-                case phys_80211_type.N24
+                case phys80211_type.N24
                     sifs = 10;
-                case phys_80211_type.N50
+                case phys80211_type.N50
                     sifs = 16;
-                case phys_80211_type.AC
+                case phys80211_type.AC
                     sifs = 16;
             end
         end
@@ -84,25 +89,25 @@ classdef phys80211 < handle
         % unit = (microseconds)
         function slotTime = SlotTime(type)
             switch(type)
-                case phys_80211_type.FHSS
+                case phys80211_type.FHSS
                     slotTime = 50;
-                case phys_80211_type.DHSS
+                case phys80211_type.DHSS
                     slotTime = 20;
-                case phys_80211_type.B
+                case phys80211_type.B
                     slotTime = 20;
-                case phys_80211_type.A
+                case phys80211_type.A
                     slotTime = 9;
-                case phys_80211_type.G_short
+                case phys80211_type.G_short
                     slotTime = 9;
-                case phys_80211_type.G_long
+                case phys80211_type.G_long
                     slotTime = 20;
-                case phys_80211_type.N24_short
+                case phys80211_type.N24_short
                     slotTime = 9;
-                case phys_80211_type.N24_long
+                case phys80211_type.N24_long
                     slotTime = 20;
-                case phys_80211_type.N50
+                case phys80211_type.N50
                     slotTime = 9;
-                case phys_80211_type.AC
+                case phys80211_type.AC
                     slotTime = 9;
             end
         end
---
+++ src/phys80211_type.m
@@ -0,0 +1,17 @@
+classdef phys_80211_type < int32
+    % Type of physical standard for radio transmissions
+    % Determines SIFS/DIFS/Transmission Rate
+    enumeration
+        Null(0)
+        FHSS(1)
+        DHSS(2)
+        B(3)
+        A(4)
+        G_short(5)
+        G_long(6)
+        N24_short(7)
+        N24_long(8)
+        N50(9)
+        AC(10)
+    end
+end
