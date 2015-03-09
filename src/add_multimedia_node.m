15c57329ab2caf453f6cf9a694a17aa1a2ca3d23
Tamir H <tamirh@gmail.com>
2015-03-09 11:51:03 -0700

    fxing typo

--- src/add_multimedia_node.m
+++ src/add_multimedia_node.m
@@ -6,19 +6,15 @@ dcf_matrix.wMin = Wmin;
 
 dcf_matrix.bFixedPacketchain = true;
 dcf_matrix.nPkt = 1;
-
-% bps = 4 * 1000000; % 4MBits/second
-% payloadSize = 1500*8;
-
 dcf_matrix.pEnterInterarrival = 1.0;
 dcf_matrix.bFixedInterarrivalChain = true;
-dcf_matrix.CalculateInterarrival(phys80211_type.B, bps, payload);
+dcf_matrix.CalculateInterarrival(phys80211_type.B, bps, payloadSize);
 
 media_matrix = markov_video_frames();
 media_matrix.gopAnchorFrameDistance = 3;
 media_matrix.gopFullFrameDistance = 12;
 media_matrix.bps = bps; 
-media_matrix.payloadSize = payload;
+media_matrix.payloadSize = payloadSize;
 
 nodeName = sprintf('media-node%d', nodeNumber);
 simulator.add_multimedia_matrix(nodeName, dcf_matrix, media_matrix);
