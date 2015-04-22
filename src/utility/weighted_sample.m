5b6b9fadc8d0c55402b179bfa9bfd92a9105878f
Tamir H <tamirh@gmail.com>
2015-04-22 13:10:01 -0700

    Fixing weighted sample function, it was not properly appending non-zero indicies to fill in the end

--- src/utility/weighted_sample.m
+++ src/utility/weighted_sample.m
@@ -15,34 +15,61 @@ classdef weighted_sample < handle
     end
     
     methods
+        % A much faster way to sample through weighted probabilities
+        % Choose a precision, and pre-divide a fixed size array
+        % Indexing into this array randomly will be a weighted probability
+        % of getting any of the values
+        % Example: weighted probabilities = [0.25 0.25 0.5], precision = 8
+        % Resulting indexing array = [1 1 2 2 3 3 3 3]
         function obj = weighted_sample(piIn)
-            precision = 10000;
+            PRECISION = 10000;
             
             obj = obj@handle();
-            %obj.pi = piIn;
-            %obj.piCumulative = cumsum(piIn)/sum(piIn);
-            obj.piIndexer = zeros(1,precision);
-            obj.piPrecision = precision;
+            obj.piIndexer = zeros(1,PRECISION);
+            obj.piPrecision = PRECISION;
+            
+            % Loop through our probabilities, mapping them onto indices
+            % Ignore 0 probability indicies
+            lastValidIndex = 1;
+            maxIndex = size(piIn,2);
             
             indexerStart = 0;
             indexerEnd = 1;
-            for i=1:size(piIn,2)
-                if (piIn(i) > 0)
-                    indexerEnd = floor( indexerStart + piIn(i) * precision );
+            for i=1:maxIndex
+                currentProbability = piIn(i);
+                if (currentProbability > 0)
+                    indexerEnd = floor( indexerStart + (currentProbability * PRECISION) );
+                    
+                    % For very small probabilities we're restricted to
+                    % 1/PRECISION chance
+                    if (indexerStart==indexerEnd)
+                        fprintf('WARN: Probability %f rounded up to %f\n', currentProbability, 1.0/PRECISION);
+                        indexerEnd = 1 + indexerStart;
+                    end
+                    
                     obj.piIndexer(indexerStart+1:indexerEnd) = i;
                     indexerStart = indexerEnd;
+                    lastValidIndex = i;
                 end
             end
             
-            %truncated = floor(piIn*precision)/precision;
-            %error = abs(piIn - truncated);
-            %errorsum = sum(error);
-
-            obj.piIndexer(indexerEnd:size(piIn,2)) = size(piIn,2);
+            % With leftover indices, map them onto the last valid index
+            obj.piIndexer(indexerEnd:PRECISION) = lastValidIndex;
+            
+            % Verify all indicies are valid
+            for i=1:PRECISION
+                index = obj.piIndexer(i);
+                if( index <= 0 || index > maxIndex || piIn(index) <= 0 )
+                    fprintf('fast indexer creation failed\n');
+                end
+                
+                assert(index > 0);
+                assert(index <= maxIndex);
+                assert(piIn(index) > 0);
+            end
         end
         
         function index = sample(this)
-            %index = find( rand <= this.piCumulative, 1 );
             index = this.piIndexer( randi(this.piPrecision) );
         end
     end
