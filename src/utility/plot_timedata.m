0227082d9882243f2779295f8abc8a658c5697c6
Tamir <tamirh@gmail.com>
2015-06-16 12:01:21 -0700

    cleaner ticks on time plots

--- src/utility/plot_timedata.m
+++ src/utility/plot_timedata.m
@@ -1,5 +1,6 @@
 function plot_timedata( figNum, subplotSize, subplotIndex, figTitle, figYLabel, labels, plotColors, nPlots, nX, data )
     figure(figNum);
+    
     ax = subplot(subplotSize(1), subplotSize(2), subplotIndex);
     hold(ax, 'on');
     
@@ -8,7 +9,20 @@ function plot_timedata( figNum, subplotSize, subplotIndex, figTitle, figYLabel, 
         plot( ax, simData, 'Color', plotColors(j, :), 'LineWidth', 1.5 );
     end
     
-    set(ax, 'XTick', 0:10:nX);
+    d = 2;
+    if (nX > 100)
+        d = 10;
+    end
+    
+    if (nX > 300)
+        d = 25;
+    end
+    
+    if (nX > 700)
+        d = 50;
+    end
+    
+    set(ax, 'XTick', 0:d:nX);
     hold(ax, 'off');
     title(ax, figTitle);
     xlabel(ax, 'Simulation Time');
